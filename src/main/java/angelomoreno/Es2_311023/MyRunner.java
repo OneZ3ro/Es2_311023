4eecf5cfc3ba5dc25d8083924378694cc150177d
Angelo Moreno <emilioangelo665@gmail.com>
2023-10-31 16:56:43 +0100

    spostato la funzione dal main al runner

--- src/main/java/angelomoreno/Es2_311023/MyRunner.java
+++ src/main/java/angelomoreno/Es2_311023/MyRunner.java
@@ -1,7 +1,9 @@
 package angelomoreno.Es2_311023;
 
+import angelomoreno.Es2_311023.entities.Ordine;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,6 +11,9 @@ import org.springframework.stereotype.Component;
 public class MyRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
-        log.info("Log proveniente da runner custom");
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Es2311023Application.class);
+        Ordine ord1 = (Ordine) ctx.getBean("ord1");
+        ord1.faiOrdine();
+        ctx.close();
     }
 }
